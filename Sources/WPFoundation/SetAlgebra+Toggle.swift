39a0dd512685f06247404464c0ada41d56f1333a
mhayes853 <matthewha853@gmail.com>
2024-06-29 03:01:13 -0700

    Set Algebra Toggle

---
+++ Sources/WPFoundation/SetAlgebra+Toggle.swift
@@ -0,0 +1,13 @@
+extension SetAlgebra {
+  /// Inserts the specified element if it does not exist in this set or removes the element
+  /// otherwise.
+  ///
+  /// - Parameter member: The member to toggle.
+  public mutating func toggle(_ member: Element) {
+    if self.contains(member) {
+      self.remove(member)
+    } else {
+      self.insert(member)
+    }
+  }
+}
