3f88c3a20b1d75ec43ad974aa725cdd679b745da
mhayes853 <matthewha853@gmail.com>
2024-07-04 16:27:07 -0700

    Some CHHapticPattern niceties

--- Sources/WPHaptics/CHHapticEngine+HapticsPlayable.swift
+++ Sources/WPHaptics/CHHapticEngine+HapticsPlayable.swift
@@ -5,15 +5,22 @@ import CoreHaptics
 
 /// A protocol for creating `CHHapticPattern`.
 public protocol CHHapticPatternConvertible {
+  /// Attempts to create a `CHHapticPattern`.
   func hapticPattern() throws -> CHHapticPattern
+}
+
+// MARK: - CHHapticPattern Conformance
+
+extension CHHapticPattern: CHHapticPatternConvertible {
+  public func hapticPattern() -> CHHapticPattern {
+    self
+  }
 }
 
 // MARK: - CHHapticEngine Conformance
 
 extension CHHapticEngine: HapticsPlayable {
-  public func play(
-    event: any CHHapticPatternConvertible
-  ) async throws {
+  public func play(event: any CHHapticPatternConvertible) async throws {
     try await self.start()
     let player = try self.makePlayer(with: event.hapticPattern())
     try player.start(atTime: CHHapticTimeImmediate)
