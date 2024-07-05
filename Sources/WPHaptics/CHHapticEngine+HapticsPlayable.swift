066d0e668148416bc6df7871f8c4bf933f39529e
mhayes853 <matthewha853@gmail.com>
2024-07-04 22:22:16 -0700

    Remove  CHHapticPatternConvertible

--- Sources/WPHaptics/CHHapticEngine+HapticsPlayable.swift
+++ Sources/WPHaptics/CHHapticEngine+HapticsPlayable.swift
@@ -1,28 +1,10 @@
 #if canImport(CoreHaptics)
 import CoreHaptics
 
-// MARK: - CHHapticPatternConvertible
-
-/// A protocol for creating `CHHapticPattern`.
-public protocol CHHapticPatternConvertible {
-  /// Attempts to create a `CHHapticPattern`.
-  func hapticPattern() throws -> CHHapticPattern
-}
-
-// MARK: - CHHapticPattern Conformance
-
-extension CHHapticPattern: CHHapticPatternConvertible {
-  public func hapticPattern() -> CHHapticPattern {
-    self
-  }
-}
-
-// MARK: - CHHapticEngine Conformance
-
 extension CHHapticEngine: HapticsPlayable {
-  public func play(event: any CHHapticPatternConvertible) async throws {
+  public func play(event: CHHapticPattern) async throws {
     try await self.start()
-    let player = try self.makePlayer(with: event.hapticPattern())
+    let player = try self.makePlayer(with: event)
     try player.start(atTime: CHHapticTimeImmediate)
   }
 }
--- Sources/WPHaptics/README.md
+++ Sources/WPHaptics/README.md
@@ -21,7 +21,3 @@ This struct describes what features of haptics that the current hardware support
 ### `HapticsPlayable`
 
 This protocol abstracts playing haptics across different platforms. It has a single associated type that represents a haptic event, and has a single method requirement to play an instance of the haptic event. This package features a WatchKit, CoreHaptics, and a Mock implementation. The mock implementation is useful for inspecting recorded haptic events dureing testing.
-
-### `CHHapticPatternConvertible`
-
-This protocol is used as the associated type requirement of the `CHHapticEngine` conformance to `HapticsPlayable`, and it has a single method in which the conforming type must return a `CHHapticPattern`.
