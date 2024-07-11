1a560f633aeea83a94c777a07f16c610370b35ad
mhayes853 <matthewha853@gmail.com>
2024-07-11 15:50:55 -0700

    Cleanup

--- Sources/WPFoundation/NSKeyValueCoding.swift
+++ Sources/WPFoundation/NSKeyValueCoding.swift
@@ -2,10 +2,10 @@ import Foundation
 
 /// A protocol to aid in writing shared abstractions between `UserDefaults` and
 /// `NSUbiquitousKeyValueStore`.
-public protocol NSDefaultsKeyValueStore: NSObject {
+public protocol DefaultsKeyValueStore: NSObject {
   func object(forKey key: String) -> Any?
   func set(_ value: Any?, forKey key: String)
 }
 
-extension UserDefaults: NSDefaultsKeyValueStore {}
-extension NSUbiquitousKeyValueStore: NSDefaultsKeyValueStore {}
+extension UserDefaults: DefaultsKeyValueStore {}
+extension NSUbiquitousKeyValueStore: DefaultsKeyValueStore {}
