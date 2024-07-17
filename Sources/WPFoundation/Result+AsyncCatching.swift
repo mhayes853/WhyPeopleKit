0c778e16b2afe82c20d584e59ab7870a2a0cbe75
mhayes853 <matthewha853@gmail.com>
2024-07-17 12:45:11 -0700

    Result+AsyncCatching

---
+++ Sources/WPFoundation/Result+AsyncCatching.swift
@@ -0,0 +1,14 @@
+extension Result where Success: ~Copyable {
+  /// Creates a new result by evaluating an async throwing closure, capturing the returned value as
+  /// a success, or any thrown error as a failure.
+  ///
+  /// - Parameter body: A throwing closure to evaluate.
+  @_alwaysEmitIntoClient
+  public init(catching body: () async throws(Failure) -> Success) async {
+    do {
+      self = .success(try await body())
+    } catch {
+      self = .failure(error)
+    }
+  }
+}
