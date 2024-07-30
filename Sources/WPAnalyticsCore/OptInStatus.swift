4e3f76c0eab3351c2ed832e59231553b15fa50e2
mhayes853 <matthewha853@gmail.com>
2024-07-29 22:35:45 -0700

    Opt in status Codable

--- Sources/WPAnalyticsCore/OptInStatus.swift
+++ Sources/WPAnalyticsCore/OptInStatus.swift
@@ -1,4 +1,4 @@
 /// A status for type for whether or not the user is opted into analytics.
-public enum OptInStatus: Hashable, Sendable {
+public enum OptInStatus: Hashable, Sendable, Codable {
   case `in`, out
 }
