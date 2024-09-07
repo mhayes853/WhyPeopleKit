644c939020e89371daf1cb4ed9ec6670cb4cf581
mhayes853 <matthewha853@gmail.com>
2024-09-06 19:45:25 -0700

    SecError message

--- Sources/WPFoundation/SecError.swift
+++ Sources/WPFoundation/SecError.swift
@@ -1,11 +1,22 @@
 import Security
 import Foundation
 
+// MARK: - SecError
+
 /// An error type for error codes returned from the Security framework.
-public struct SecError: Error {
+public struct SecError: Error, Hashable, Sendable {
   public let code: OSStatus
   
   public init(code: OSStatus) {
     self.code = code
+  }
+}
+
+// MARK: - Message
+
+extension SecError {
+  /// A human readable error message string from this error.
+  public var message: String {
+    SecCopyErrorMessageString(self.code, nil) as? String ?? "Unknown Security Framework Error"
   }
 }
