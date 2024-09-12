13bc707ab842fc1e6cf96486fab4ef2405bbe02f
mhayes853 <matthewha853@gmail.com>
2024-09-12 12:04:32 -0700

    Add docks

--- Sources/WPGRDB/SingleRowTable.swift
+++ Sources/WPGRDB/SingleRowTable.swift
@@ -2,6 +2,7 @@ import GRDB
 
 // MARK: - SingleRowTableRecord
 
+/// A type that is stored as the sole row of a table.
 public protocol SingleRowTableRecord: FetchableRecord, PersistableRecord, TableRecord {
   init()
 }
@@ -13,10 +14,19 @@ extension SingleRowTableRecord {
     }
   }
   
+  /// Returns the sole record from the database.
+  ///
+  /// - Parameter db: A database connection.
   public static func find(_ db: Database) throws -> Self {
     try Self.fetchOne(db) ?? Self()
   }
   
+  /// Performs an update on the sole record from the database.
+  ///
+  /// - Parameters:
+  ///   - db: A database connection.
+  ///   - update: A closure to update the record.
+  /// - Returns: Whatever `update` returns.
   public static func update<T>(_ db: Database, update: (inout Self) throws -> T) throws -> T {
     var record = try Self.find(db)
     let value = try update(&record)
@@ -28,6 +38,12 @@ extension SingleRowTableRecord {
 // MARK: - TableDefinition
 
 extension TableDefinition {
+  /// Appends a primary key column that only allows a single record to be stored in this table.
+  ///
+  /// The value of the primary key must always be an integer with a value equal to 1.
+  ///
+  /// - Parameter name: The column name.
+  /// - Returns: A ``ColumnDefinition`` that allows you to refine the column definition.
   @discardableResult
   public func singleRowTablePrimaryKey(_ name: String) -> ColumnDefinition {
     self.primaryKey(name, .integer, onConflict: .replace)
