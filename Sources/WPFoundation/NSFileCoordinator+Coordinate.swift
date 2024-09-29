7487bce8da7b9b219fb331056c0898ca55bbad76
mhayes853 <matthewha853@gmail.com>
2024-09-28 22:01:37 -0700

    More NSFileCoordinator conveniences

--- Sources/WPFoundation/NSFileCoordinator+Coordinate.swift
+++ Sources/WPFoundation/NSFileCoordinator+Coordinate.swift
@@ -8,20 +8,11 @@ extension NSFileCoordinator {
     options: NSFileCoordinator.WritingOptions = [],
     byAccessor: (URL) throws -> T
   ) throws -> T {
-    var value: T?
-    var coordinatorError: NSError?
-    var operationError: (any Error)?
-    self.coordinate(writingItemAt: url, options: options, error: &coordinatorError) { url in
-      do {
-        value = try byAccessor(url)
-      } catch {
-        operationError = error
+    try self.coordinate { pointer, state in
+      self.coordinate(readingItemAt: url, error: pointer) { url in
+        state.perform { try byAccessor(url) }
       }
     }
-    if let error = coordinatorError ?? operationError {
-      throw error
-    }
-    return value!
   }
 }
 
@@ -33,19 +24,87 @@ extension NSFileCoordinator {
     options: NSFileCoordinator.ReadingOptions = [],
     byAccessor: (URL) throws -> T
   ) throws -> T {
-    var value: T?
-    var coordinatorError: NSError?
-    var operationError: (any Error)?
-    self.coordinate(readingItemAt: url, options: options, error: &coordinatorError) { url in
-      do {
-        value = try byAccessor(url)
-      } catch {
-        operationError = error
+    try self.coordinate { pointer, state in
+      self.coordinate(readingItemAt: url, error: pointer) { url in
+        state.perform { try byAccessor(url) }
       }
     }
-    if let error = coordinatorError ?? operationError {
+  }
+}
+
+// MARK: - Reading and Writing
+
+extension NSFileCoordinator {
+  public func coordinate<T>(
+    readingItemAt: URL,
+    options readingOptions: NSFileCoordinator.ReadingOptions,
+    writingItemAt: URL,
+    options writingOptions: NSFileCoordinator.WritingOptions,
+    byAccessor: (URL, URL) throws -> T
+  ) throws -> T {
+    try self.coordinate { pointer, state in
+      self.coordinate(
+        readingItemAt: readingItemAt,
+        options: readingOptions,
+        writingItemAt: writingItemAt,
+        options: writingOptions,
+        error: pointer
+      ) { url1, url2 in
+        state.perform { try byAccessor(url1, url2) }
+      }
+    }
+  }
+}
+
+// MARK: - Writing and Writing
+
+extension NSFileCoordinator {
+  public func coordinate<T>(
+    writingItemAt url1: URL,
+    options options1: NSFileCoordinator.WritingOptions,
+    writingItemAt url2: URL,
+    options options2: NSFileCoordinator.WritingOptions,
+    byAccessor: (URL, URL) throws -> T
+  ) throws -> T {
+    try self.coordinate { pointer, state in
+      self.coordinate(
+        writingItemAt: url1,
+        options: options1,
+        writingItemAt: url2,
+        options: options2,
+        error: pointer
+      ) { url1, url2 in
+        state.perform { try byAccessor(url1, url2) }
+      }
+    }
+  }
+}
+
+// MARK: - Helper
+
+private struct CoordinateState<T> {
+  private(set) var value: T?
+  private(set) var error: (any Error)?
+  
+  mutating func perform(_ work: () throws -> T) {
+    do {
+      self.value = try work()
+    } catch {
+      self.error = error
+    }
+  }
+}
+
+extension NSFileCoordinator {
+  private func coordinate<T>(
+    _ coordinate: (NSErrorPointer, inout CoordinateState<T>) throws -> Void
+  ) throws -> T {
+    var state = CoordinateState<T>()
+    var coordinatorError: NSError?
+    try coordinate(&coordinatorError, &state)
+    if let error = coordinatorError ?? state.error {
       throw error
     }
-    return value!
+    return state.value!
   }
 }
