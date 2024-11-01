84c9914ca479b77ab46dd41f75f75e35dd90794a
mhayes853 <matthewha853@gmail.com>
2024-11-01 02:58:43 -0700

    Local network private IPV4 Address

---
+++ Tests/WPFoundationTests/IPV4AddressTests.swift
@@ -0,0 +1,11 @@
+import Testing
+import WPFoundation
+
+@Suite("IPV4Address tests")
+struct IPV4AddressTests {
+  @Test("Local Private Address")
+  func localPrivateIP() throws {
+    let address = try #require(IPV4Address.localPrivate)
+    #expect(address.description.starts(with: "10.0.0"))
+  }
+}
