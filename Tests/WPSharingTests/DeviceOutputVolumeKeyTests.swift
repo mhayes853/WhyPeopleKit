cff225dff0d26e25332eb0740951d1861ede15d4
mhayes853 <matthewha853@gmail.com>
2024-12-23 22:46:51 -0800

    Linux guard tests

--- Tests/WPSharingTests/DeviceOutputVolumeKeyTests.swift
+++ Tests/WPSharingTests/DeviceOutputVolumeKeyTests.swift
@@ -1,25 +1,27 @@
-import Testing
-import WPDependencies
-import WPDeviceVolume
-import WPSharing
+#if !os(linux)
+  import Testing
+  import WPDependencies
+  import WPDeviceVolume
+  import WPSharing
 
-@Suite("DeviceOutputVolumeKey tests")
-struct DeviceOutputVolumeKeyTests {
-  @Test("Consumes Sequence of Status Updates")
-  func consumeStatusUpdates() async {
-    let outputVolume = TestDeviceOutputVolume()
-    await withDependencies {
-      $0.systemDeviceOutputVolume = outputVolume
-    } operation: {
-      @SharedReader(.systemDeviceOutputVolume) var volume
+  @Suite("DeviceOutputVolumeKey tests")
+  struct DeviceOutputVolumeKeyTests {
+    @Test("Consumes Sequence of Status Updates")
+    func consumeStatusUpdates() async {
+      let outputVolume = TestDeviceOutputVolume()
+      await withDependencies {
+        $0.systemDeviceOutputVolume = outputVolume
+      } operation: {
+        @SharedReader(.systemDeviceOutputVolume) var volume
 
-      var status = DeviceOutputVolumeStatus(outputVolume: 0.5)
-      await outputVolume.send(result: .success(status))
-      #expect(volume == status)
+        var status = DeviceOutputVolumeStatus(outputVolume: 0.5)
+        await outputVolume.send(result: .success(status))
+        #expect(volume == status)
 
-      status = DeviceOutputVolumeStatus(outputVolume: 0.2)
-      await outputVolume.send(result: .success(status))
-      #expect(volume == status)
+        status = DeviceOutputVolumeStatus(outputVolume: 0.2)
+        await outputVolume.send(result: .success(status))
+        #expect(volume == status)
+      }
     }
   }
-}
+#endif
