7654e1ed8d96caf26456217cc169fb1d61b69cf3
mhayes853 <matthewha853@gmail.com>
2025-01-07 15:16:43 -0800

    RecursiveLock

--- Sources/WPFoundation/Lock.swift
+++ Sources/WPFoundation/Lock.swift
@@ -29,7 +29,7 @@ extension Lock {
   /// > Warning: Do not call this method from within `body`, this will cause a deadlock.
   ///
   /// - Parameter body: A closure with mutable access to the underlying value.
-  /// - Returns:
+  /// - Returns: Whatever `body` returns.
   public borrowing func withLock<Result: ~Copyable, E: Error>(
     _ body: (inout sending Value) throws(E) -> sending Result
   ) throws(E) -> sending Result {
@@ -42,3 +42,44 @@ extension Lock {
 // MARK: - Sendable
 
 extension Lock: @unchecked Sendable where Value: ~Copyable {}
+
+// MARK: - RecursiveLock
+
+/// A simple lock data type that is also recursive.
+///
+/// This type behaves like `Mutex`, but allows threads that have acquired the lock to acquire it
+/// again.
+public struct RecursiveLock<Value: ~Copyable>: ~Copyable {
+  private let lock = NSRecursiveLock()
+  private var value: UnsafeMutablePointer<Value>
+
+  /// Creates a lock by consuming the specified value.
+  ///
+  /// - Parameter value: The initial value of the lock.
+  public init(_ value: consuming sending Value) {
+    self.value = UnsafeMutablePointer<Value>.allocate(capacity: 1)
+    self.value.initialize(to: value)
+  }
+
+  deinit { self.value.deallocate() }
+}
+
+// MARK: - WithLock
+
+extension RecursiveLock {
+  /// Calls the specified closure with the lock acquired and gives up ownership of the value.
+  ///
+  /// - Parameter body: A closure with mutable access to the underlying value.
+  /// - Returns: Whatever `body` returns.
+  public borrowing func withLock<Result: ~Copyable, E: Error>(
+    _ body: (inout sending Value) throws(E) -> sending Result
+  ) throws(E) -> sending Result {
+    self.lock.lock()
+    defer { self.lock.unlock() }
+    return try body(&self.value.pointee)
+  }
+}
+
+// MARK: - Sendable
+
+extension RecursiveLock: @unchecked Sendable where Value: ~Copyable {}
--- Tests/WPFoundationTests/LockTests.swift
+++ Tests/WPFoundationTests/LockTests.swift
@@ -13,6 +13,34 @@ struct LockTests {
     }
     #expect(counter.count == 100)
   }
+
+  @Test("Recursive Concurrent Increment")
+  func recursiveConcurrentIncrement() async {
+    let counter = RecursiveCounter()
+    await withTaskGroup(of: Void.self) { group in
+      for _ in 0..<100 {
+        group.addTask { counter.incrementTwice() }
+      }
+    }
+    #expect(counter.count == 200)
+  }
+}
+
+private final class RecursiveCounter: Sendable {
+  private let _count = RecursiveLock(0)
+
+  var count: Int {
+    self._count.withLock { $0 }
+  }
+
+  func incrementTwice() {
+    self._count.withLock {
+      $0 += 1
+      self._count.withLock {
+        $0 += 1
+      }
+    }
+  }
 }
 
 private final class Counter: Sendable {
