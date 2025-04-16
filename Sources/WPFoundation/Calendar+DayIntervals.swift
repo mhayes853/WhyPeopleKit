262942d1368b55f39c8b038dd80f5d4cd89ca515
mhayes853 <matthewha853@gmail.com>
2025-04-15 17:32:48 -0700

    Docs

--- Sources/WPFoundation/Calendar+DayIntervals.swift
+++ Sources/WPFoundation/Calendar+DayIntervals.swift
@@ -1,10 +1,11 @@
 import Foundation
 
 extension Calendar {
+  /// Splits the specified `interval` into individual day intervals.
+  ///
+  /// - Parameter interval: The date interval to split.
+  /// - Returns: An array of date intervals representing each day within the specified interval.
   public func dayIntervals(from interval: DateInterval) -> [DateInterval] {
-    //if self.isDate(interval.start, inSameDayAs: interval.end) {
-    //  return [interval]
-    //}
     var intervals = [DateInterval]()
     var currentDate = interval.start
     while currentDate < interval.end {
