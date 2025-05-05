7f90d337d1fa92010df9e9407abb92d009baeac2
mhayes853 <matthewha853@gmail.com>
2025-05-05 11:30:26 -0700

    Update Docs

--- Sources/WPFoundation/Clock+MeasureWithValue.swift
+++ Sources/WPFoundation/Clock+MeasureWithValue.swift
@@ -1,21 +1,34 @@
 @available(iOS 16.0, macOS 13.0, watchOS 9.0, tvOS 16.0, *)
 extension Clock {
+  /// Measure the elapsed time to execute an asynchronous closure.
+  ///
+  ///       let clock = ContinuousClock()
+  ///       let (elapsed, returnValue) = await clock.measureWithValue {
+  ///          await someWork()
+  ///       }
   public func measureWithValue<T>(
-    _ action: () async throws -> T
+    isolation: isolated (any Actor)? = #isolation,
+    _ work: () async throws -> T
   ) async rethrows -> (Duration, T) {
     var value: T?
     let time = try await self.measure {
-      value = try await action()
+      value = try await work()
     }
     return (time, value!)
   }
 
+  /// Measure the elapsed time to execute a closure.
+  ///
+  ///       let clock = ContinuousClock()
+  ///       let (elapsed, returnValue) = clock.measureWithValue {
+  ///          someWork()
+  ///       }
   public func measureWithValue<T>(
-    _ action: () throws -> T
+    _ work: () throws -> T
   ) rethrows -> (Duration, T) {
     var value: T?
     let time = try self.measure {
-      value = try action()
+      value = try work()
     }
     return (time, value!)
   }
